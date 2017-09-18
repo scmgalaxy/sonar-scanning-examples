e13148efd8b6ca61f38477551c51c77349dd1a6b
Rajesh GLOBE <DevOps@RajeshKumar.xyz>
2017-09-18 14:20:27 +0530

    adding first

---
+++ sonar-examples-java/README.md
@@ -0,0 +1,12 @@
+## Configuration of Projects
+
+The directory /projects contains sample projects for:
+ * the different analyzers (SonarQube Runner, Maven, Ant)
+ * the different languages (Java, Cobol, .Net, etc.)
+ * the different ways to execute unit tests and get code coverage data
+
+## SonarQube Plugins
+
+The directory /plugins contains the most frequent kinds of custom plugins:
+ * Extensions of rule engines like Java, JavaScript and PHP
+ * Define a new metric, compute measures and display them in web app
---
+++ sonar-examples-java/plugins/sonar-example-plugin/src/main/java/org/sonarsource/plugins/example/rules/FooLintRulesDefinition.java
@@ -0,0 +1,49 @@
+package org.sonarsource.plugins.example.rules;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.api.server.rule.RulesDefinitionXmlLoader;
+import org.sonarsource.plugins.example.languages.FooLanguage;
+
+public final class FooLintRulesDefinition implements RulesDefinition {
+
+  private static final String PATH_TO_RULES_XML = "/example/foolint-rules.xml";
+
+  protected static final String KEY = "foolint";
+  protected static final String NAME = "FooLint";
+
+  protected String rulesDefinitionFilePath() {
+    return PATH_TO_RULES_XML;
+  }
+
+  private void defineRulesForLanguage(Context context, String repositoryKey, String repositoryName, String languageKey) {
+    NewRepository repository = context.createRepository(repositoryKey, languageKey).setName(repositoryName);
+
+    InputStream rulesXml = this.getClass().getResourceAsStream(rulesDefinitionFilePath());
+    if (rulesXml != null) {
+      RulesDefinitionXmlLoader rulesLoader = new RulesDefinitionXmlLoader();
+      rulesLoader.load(repository, rulesXml, StandardCharsets.UTF_8.name());
+    }
+
+    repository.done();
+  }
+
+  @Override
+  public void define(Context context) {
+    String repositoryKey = FooLintRulesDefinition.getRepositoryKeyForLanguage(FooLanguage.KEY);
+    String repositoryName = FooLintRulesDefinition.getRepositoryNameForLanguage(FooLanguage.KEY);
+    defineRulesForLanguage(context, repositoryKey, repositoryName, FooLanguage.KEY);
+  }
+
+  public static String getRepositoryKeyForLanguage(String languageKey) {
+    return languageKey.toLowerCase(Locale.ENGLISH) + "-" + KEY;
+  }
+
+  public static String getRepositoryNameForLanguage(String languageKey) {
+    return languageKey.toUpperCase(Locale.ENGLISH) + " " + NAME;
+  }
+
+}
